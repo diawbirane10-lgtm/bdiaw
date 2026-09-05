8f78ad8b6f84af22ace7b2116a9b4b3c9ac3cf4d
Galahad <diawbirane10@gmail.com>
2026-09-05 06:02:10 +0000

    Add robots.txt metadata route

---
+++ recovered-ohmega/app/robots.js
@@ -0,0 +1,12 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://b-diaw.com/sitemap.xml',
+    host: 'https://b-diaw.com',
+  };
+}
