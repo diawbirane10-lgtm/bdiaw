4f3011be5eaa73b5fefa224ca4ad7827caaf2f40
Galahad <diawbirane10@gmail.com>
2026-09-05 13:02:13 +0000

    Keep private routes out of search crawling

--- recovered-ohmega/app/robots.js
+++ recovered-ohmega/app/robots.js
@@ -4,6 +4,7 @@ export default function robots() {
       {
         userAgent: '*',
         allow: '/',
+        disallow: ['/admin', '/admin/', '/api/', '/_next/'],
       },
     ],
     sitemap: 'https://b-diaw.com/sitemap.xml',
