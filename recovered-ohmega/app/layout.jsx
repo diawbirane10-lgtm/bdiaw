70b65b0cbcdebf011778d28935af4abf572e0de8
Galahad <diawbirane10@gmail.com>
2026-09-13 22:56:31 +0000

    Mount animated intro splash

--- recovered-ohmega/app/layout.jsx
+++ recovered-ohmega/app/layout.jsx
@@ -9,6 +9,7 @@ import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import SiteAnalytics from './SiteAnalytics';
 import EventsSection from './EventsSection';
+import IntroSplash from './IntroSplash';
 
 const siteUrl = 'https://b-diaw.com';
 
@@ -139,6 +140,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body>
+        <IntroSplash />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(profileJsonLd) }}
